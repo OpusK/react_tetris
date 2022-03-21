693cc0a452f1230e083d95d0d01ea4f37d710d65
Kei <opusk44@gmail.com>
2022-03-21 23:59:37 +0900

    add Game component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Game from './components/Game';
 
 function App() {
   return (
     <div className="App">
       <h1>Let's make Ketris</h1>
+      <Game rows={20} columns={10} />
     </div>
   );
 }
