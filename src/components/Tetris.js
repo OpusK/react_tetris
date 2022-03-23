d4de510db2305177d82bc13c5044fd545ad3df74
Kei <opusk44@gmail.com>
2022-03-23 20:45:46 +0900

    create Tetris component (init state)

---
+++ src/components/Tetris.js
@@ -0,0 +1,15 @@
+import "./Tetris.css";
+
+const Tetris = ({ rows, columns, setGameOver }) => {
+  return (
+    <div className="Tetris">
+      {/* Board */}
+      {/* GameStats */}
+      {/* Previews */}
+      {/* GameController */}
+      <p>Tetris Game!!</p>
+    </div>
+  );
+};
+
+export default Tetris;
