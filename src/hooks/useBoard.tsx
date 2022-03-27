6400cd1b1e5b741cf75f9115e1426268dd85b025
Kei <opusk44@gmail.com>
2022-03-27 22:42:50 +0900

    Create useBoard hook

--- src/hooks/useBoard.tsx
+++ src/hooks/useBoard.tsx
@@ -1,9 +1,28 @@
 import { useState, useEffect } from "react";
-import { buildBoard } from "business/Board";
-import { BoardSize } from "typings";
+import { buildBoard, nextBoard } from "business/Board";
+import { BoardSize, Player, TetrisBoard } from "typings";
 
-export const useBoard = ({rows, columns}: BoardSize) => {
-  const [board, setBoard] = useState(buildBoard({ rows, columns }));
+type Props = {
+  rows: number,
+  columns: number,
+  player: Player,
+  resetPlayer: () => void,
+  addLinesCleared: (lines: number) => void,
+}
+
+export const useBoard = ({rows, columns, player, resetPlayer, addLinesCleared}: Props) => {
+  const [board, setBoard] = useState<TetrisBoard>(buildBoard({ rows, columns }));
+
+  useEffect(() => {
+    setBoard((previousBoard) =>
+      nextBoard({
+        board: previousBoard,
+        player,
+        resetPlayer,
+        addLinesCleared
+      })
+    );
+  }, [player, resetPlayer, addLinesCleared]);
 
   return [board];
 };
