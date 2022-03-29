1d14080843fe12335e5d292585d42c270d934f05
Kei <opusk44@gmail.com>
2022-03-30 00:45:52 +0900

    add hooks to drop block & to callback in time interval

---
+++ src/hooks/useInterval.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useRef } from "react";
+
+type Props = {
+  callback: () => void;
+  delay: number;
+}
+
+export const useInterval = ({ callback, delay }: Props) => {
+  const savedCallback = useRef<() => void>();
+
+  // Remember the latest callback
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval
+  useEffect(() => {
+    function tick() {
+      if (savedCallback != null && savedCallback.current != null) {
+        savedCallback.current();
+      }
+    }
+    if (delay !== null) {
+      const id = setInterval(tick, delay);
+      return () => {
+        clearInterval(id);
+      };
+    }
+  }, [delay]);
+};
