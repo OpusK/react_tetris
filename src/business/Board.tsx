1e4f00ca39bbc0202f9cb26da59417ab212ee6d4
Kei <opusk44@gmail.com>
2022-03-29 22:31:23 +0900

    add physics function (collision & bounded box)

--- src/business/Board.tsx
+++ src/business/Board.tsx
@@ -1,4 +1,4 @@
-import { BoardSize, TetrisBoard, Player } from "typings";
+import { BoardSize, TetrisBoard, Player, TetrominoState } from "typings";
 import { transferToBoard } from "business/Tetrominoes";
 
 const defaultCell = {
@@ -46,4 +46,43 @@ export const nextBoard = ({ board, player, resetPlayer, addLinesCleared }: Props
     rows,
     size: { ...board.size }
   };
+};
+
+export const hasCollision = ({ board, position, shape }: TetrominoState) => {
+  for (let y = 0; y < shape.length; y++) {
+    const row = y + position.row;
+
+    for (let x = 0; x < shape[y].length; x++) {
+      if (shape[y][x]) {
+        const column = x + position.column;
+
+        if (
+          board.rows[row] &&
+          board.rows[row][column] &&
+          board.rows[row][column].occupied
+        ) {
+          return true;
+        }
+      }
+    }
+  }
+
+  return false;
+};
+
+export const isWithinBoard = ({ board, position, shape }: TetrominoState) => {
+  for (let y = 0; y < shape.length; y++) {
+    const row = y + position.row;
+
+    for (let x = 0; x < shape[y].length; x++) {
+      if (shape[y][x]) {
+        const column = x + position.column;
+        const isValidPosition = board.rows[row] && board.rows[row][column];
+
+        if (!isValidPosition) return false;
+      }
+    }
+  }
+
+  return true;
 };
